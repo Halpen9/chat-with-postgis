18142a0b4fe3941f62b8a3f579124c9ebf5f277a
Halpen9 <leo.maquintestud@gmail.com>
2025-12-04 14:07:51 +0100

    création de fichier intermédiaire pour commit

---
+++ deployappL.py
@@ -0,0 +1,240 @@
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.utilities import SQLDatabase
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from openai import OpenAI
+from PIL import Image
+from langchain.schema import AIMessage
+from datetime import datetime
+from langsmith import traceable,Client
+
+import streamlit as st
+import os
+import matplotlib.pyplot as plt 
+import io
+import base64
+import traceback 
+import imghdr
+
+
+now = datetime.now()
+
+# LangChain
+os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
+
+# OPENAI
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
+host = st.secrets["postgres"]["host"]
+port = st.secrets["postgres"]["port"]
+user = st.secrets["postgres"]["user"]
+password = st.secrets["postgres"]["password"]
+database = st.secrets["postgres"]["database"]
+
+
+client = Client()
+
+def init_database()-> SQLDatabase:
+    #db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+    db_uri = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
+
+def get_sql_chain(db):
+    template = """
+Tu es un data analyst travaillant pour une entreprise.
+Tu échanges avec un utilisateur qui te pose des questions sur la base de données spatial (postgis) de l'entreprise.
+
+À partir du schéma des tables ci-dessous, écris une requête SQL qui permettrait de répondre à la question de l'utilisateur.
+Tiens également compte de l'historique de la conversation pour formuler ta réponse.
+
+Si la question concerne la temporalité, la date actuelle est : {current_date}.
+
+⚠️ IMPORTANT — RÈGLES POUR SUPABASE :
+- N'utilise JAMAIS ST_DistanceSphere().
+- Pour calculer des distances réelles en mètres, utilise : 
+  ST_Distance(geom::geography, geom::geography)
+- Pour calculer un rayon autour d’un point, utilise aussi ST_Distance(...::geography).
+- Toujours caster les géométries en ::geography avant ST_Distance.
+- Toujours renvoyer une REQUÊTE SQL VALIDE SUPABASE.
+
+<SCHEMA>{schema}</SCHEMA>
+
+Historique de la conversation : {chat_history}
+
+Rédige uniquement la requête SQL — sans aucun texte explicatif, sans commentaire et sans backticks.
+
+Exemple :
+Question : Trouver les bureaux dans un rayon de 100 km autour de Paris ?
+Requête SQL : 
+SELECT o.name, c.name AS city
+FROM offices o
+JOIN cities c ON o.city_id = c.id
+WHERE ST_DistanceSphere(o.geom, ST_GeomFromText('POINT(2.3522 48.8566)', 4326)) < 100000;
+
+
+Question : Trouver les clients à moins de 50 km du bureau de Lyon.
+Requête SQL : 
+SELECT cl.name, cl.revenue
+FROM clients cl
+JOIN offices o ON cl.office_id = o.id
+WHERE o.name = 'Lyon Center'
+AND ST_DistanceSphere(cl.geom, o.geom) < 50000;
+
+Question : Calculer la distance entre Paris et Marseille.
+Requête SQL : 
+SELECT ST_DistanceSphere(
+    (SELECT geom FROM cities WHERE name = 'Paris'),
+    (SELECT geom FROM cities WHERE name = 'Marseille')
+) / 1000 AS distance_km;
+
+À ton tour :
+
+Question : {question}
+Requête SQL :
+"""
+    prompt = ChatPromptTemplate.from_template(template)
+
+    #llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+    llm= ChatOpenAI(model= "gpt-4o-mini")
+    def get_schema(_):
+        return db.get_table_info()
+    
+    return (
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt
+        | llm
+        | StrOutputParser()
+
+    )
+    
+def get_rep(user_query: str, chat_history: list):
+    prompt = """
+    Tu es un spécialiste dans le sujet de la base de donnée qui est à ta disposition. Analyse la demande utilisateur et réponds UNIQUEMENT par :
+    - "sql" si la question nécessite une requête SQL sur la base
+    - "image" si l'utilisateur veut une image, carte, schéma, visualisation
+    - "chat" pour toute réponse en langage naturel
+    Historique : {chat_history}
+    Question : {question}
+    Réponse :"""
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    prompt = ChatPromptTemplate.from_template(prompt)
+    chain = prompt | llm | StrOutputParser()
+    reponsee = chain.invoke({"question": user_query, "chat_history": chat_history})
+    return reponsee.strip().lower()
+
+def get_response(user_query : str, db: SQLDatabase, chat_history: list):
+    sql_chain = get_sql_chain(db)
+
+    template = """
+    Tu es un data analyst travaillant pour une entreprise.  
+Tu échanges avec un utilisateur qui te pose des questions sur la base de données spatial (postgis) de l'entreprise.
+
+En te basant sur :
+- le schéma des tables ci-dessous,  
+- la question de l'utilisateur,  
+- la requête SQL générée,  
+- et le résultat de cette requête,  
+
+rédige une **réponse claire et naturelle** en français, adaptée à l'utilisateur. Donne aussi la requete sql en fin de réponse.
+
+<SCHEMA>{schema}</SCHEMA>
+
+Historique de la conversation : {chat_history}  
+Requête SQL : <SQL>{query}</SQL>  
+Question de l'utilisateur : {question}  
+Résultat SQL : {response}"""
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    #llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+    llm= ChatOpenAI(model= "gpt-4o-mini")
+
+    chain = (
+        RunnablePassthrough.assign(query=sql_chain).assign(
+            schema=lambda _: db.get_table_info(),
+            response=lambda vars : db.run(vars["query"]),
+        )
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return chain.invoke({
+        "question": user_query,
+        "chat_history": chat_history,
+        "current_date": now,
+
+    })
+
+def display_schema(db: SQLDatabase):
+    def get_schema(_):
+        return db.get_table_info()
+    template = """
+    Voici le schéma des tables de la base de données :
+    <SCHEMA>{schema}</SCHEMA>
+    Rédige une courte et concise présentation de cette base de données en français. Pas besoin d'exemples ou de détails techniques.
+    Présente la de façon claire, structurée et ergonomique.
+    Par exemple, noms des tables et colonnes avec une courte description en langage naturel.
+    """
+    prompt= ChatPromptTemplate.from_template(template)
+    llm= ChatOpenAI(model= "gpt-4o-mini")
+    return (
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Bonjour! Je suis un assistant SQL. Demande moi ce que tu veux sur ta base de données")
+    ]
+if "schema_display" not in st.session_state:
+    st.session_state.schema_display = None
+
+
+
+st.set_page_config(page_title="Discute avec ta base de données", page_icon=":speech_balloon:")
+st.title("Discute avec ta base de données")
+
+with st.sidebar:
+    st.subheader("Paramètres")
+    st.write("C'est une simple application de discussion utilisant SQL. Connectez vous à la base de données pour commencer la discussion")
+    if st.button("Connection"):
+        with st.spinner("Connection à la base de données..."):
+            db = init_database()
+            st.session_state.db=db
+            st.success("Connecté à la base de données!")
+            st.session_state.schema_display = display_schema(st.session_state.db).invoke({})
+    if st.session_state.schema_display:
+        st.markdown(st.session_state.schema_display)
+
+
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Humain"):
+            st.markdown(message.content)
+
+user_query = st.chat_input("Ecrivez un message...")
+if user_query is not None and user_query.strip() != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+    
+    with st.chat_message("AI"):
+        response = get_response(user_query,st.session_state.db, st.session_state.chat_history)
+        st.markdown(response)
+
+    st.session_state.chat_history.append(AIMessage(content=response))
+
